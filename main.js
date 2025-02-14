bd35002fcd3874c3c99745f7465b3879ab812f77
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-02-14 13:47:55 +0300

    add code for activating burger menu

---
+++ main.js
@@ -0,0 +1,17 @@
+addNavMenu();
+
+function addNavMenu() {
+    const button = document.querySelector('#header-burger');
+    const nav = document.querySelector('#header-nav');
+    const body = document.body
+
+    button.addEventListener('click', function(event) {
+        const elementsToActive = [button, nav];
+        elementsToActive.forEach(toggleActiveClass);
+        body.classList.toggle('lock');
+    })
+}
+
+function toggleActiveClass(element) {
+    element.classList.toggle('active');
+}
